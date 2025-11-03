12ad17ce7c6340b17578b733d9c191f13437a27f
Cyber-Eder <eder@cybersoftsistemas.com.br>
2025-11-03 09:42:40 -0300

    remove all

--- Script SQL/Menu Principal.sql
+++
@@ -1,6 +0,0 @@
-update MenuPrincipal set Form = 'TfCadEmpresas' where id = 3
-update MenuPrincipal set Form = 'TfCadUsuarios' where id = 4
-update MenuPrincipal set Form = 'TfCadPaises'   where id = 5
-
-
-select * from MenuPrincipal order by id, ParentId
